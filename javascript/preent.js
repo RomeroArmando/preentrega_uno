22412005c4a3b009d049290dcca0d3d1e99570db
Romero Armando <romeroarmando094@gmail.com>
2023-12-27 10:35:55 -0300

    update de carrito

--- javascript/preent.js
+++ javascript/preent.js
@@ -1,5 +1,4 @@
 let cantidadCompras = 0;
-let cantidadProductos = 0;
 let totalHoy = 0;
 
 let mensaje;
@@ -18,66 +17,74 @@ let precio_5 = 0;
 
 let tag;
 
-let Total = 0;
+let total = 0;
 
 
-function check(prod, precio, q) {
-    if (prod != null || !isNaN(precio)) {
-        if (prod == "" || prod.length < 3) {
+function check_text(prod, q) {
+
+    if (prod != null) {
+
+        if (prod == null || prod == "" || prod.length < 3) {
 
             alert("ingrese un nombre real");
             prods(q);
 
         }
-
-        if (isNaN(precio)) {
-            alert("ingrese un valor numerico");
-            prods(q);
-        }
     }
 }
 
+function check_num(precio, q) {
+
+    if (isNaN(precio)) {
+
+        alert("ingrese un valor numerico");
+        prods(q);
+
+    }
+
+}
 
 function prods(a) {
-
 
 
     if (a == 1) {
 
         producto_1 = prompt("ingrese nombre de producto " + a);
-        check(producto_1, 99, a);
+        check_text(producto_1, a);
         precio_1 = parseFloat(prompt("ingrese valor del producto " + a));
-        check(producto_1, precio_1, a);
+        check_num(precio_1, a);
+        console.log(precio_1);
 
     } else if (a == 2) {
 
         producto_2 = prompt("ingrese nombre de producto " + a);
-        console.log(producto_2);
-        check(producto_2, 99, a);
+        //        console.log(producto_2);
+        check_text(producto_2, a);
         precio_2 = parseFloat(prompt("ingrese valor del producto " + a));
-        console.log(precio_2 );
-        check(producto_2, precio_2, a);
+        check_num(precio_2, a);
+
+        //        check(producto_2, precio_2, a);
 
     } else if (a == 3) {
 
         producto_3 = prompt("ingrese nombre de producto " + a);
-        check(producto_3, 99, a);
+        check_text(producto_3, a);
         precio_3 = parseFloat(prompt("ingrese valor del producto " + a));
-        check(producto_3, precio_3, a);
+        check_num(precio_3, a);
 
     } else if (a == 4) {
 
         producto_4 = prompt("ingrese nombre de producto " + a);
-        check(producto_4, 99, a);
+        check_text(producto_4, a);
         precio_4 = parseFloat(prompt("ingrese valor del producto " + a));
-        check(producto_4, precio_4, a);
+        check_num(precio_4, a);
 
     } else if (a == 5) {
 
         producto_5 = prompt("ingrese nombre de producto " + a);
-        check(producto_5, 99, a);
+        check_text(producto_5, a);
         precio_5 = parseFloat(prompt("ingrese valor del producto " + a));
-        check(producto_5, precio_5, a);
+        check_num(precio_5, a);
 
     }
 
@@ -85,45 +92,23 @@ function prods(a) {
 
 function carrito() {
 
-    for (let i = 1; i < 6; i++) {
+    let i = 1;
+
+    do {
 
         prods(i);
 
-        if (i == 1) {
-
-            if (producto_1 == null || isNaN(precio_1)) {
-                i = 6;
-            }
-
-        } else if (i == 2) {
-
-            if (producto_2 == null || isNaN(precio_2)) {
-                i = 6;
-            }
-
-        } else if (i == 3) {
-
-            if (producto_3 == null || isNaN(precio_3)) {
-                i = 6;
-            }
-
-        } else if (i == 4) {
-
-            if (producto_4 == null || isNaN(precio_4)) {
-                i = 6;
-            }
-
-        } else if (i == 5) {
-
-            if (producto_5 == null || isNaN(precio_5)) {
-                i = 6;
-            }
-
-        }
-
         console.log(i);
 
-    }
+        i++;
+
+    } while (i < 6)
+
+    total = precio_1 + precio_2 + precio_3 + precio_4 + precio_5;
+
+    totalHoy = totalHoy + total;
+
+    cantidadCompras++;
 
 }
 
@@ -147,7 +132,7 @@ function compra() {
 
 function menu() {
 
-    mensaje = parseInt(prompt("seleccione una opcion" + "\n🛒 Compra: 1" + "\n💵 Total de compras hoy: 2" + "\n🎈 Valor de ultima compra: 3" + "\n🧲 cantidad de compras: 4" + "\n♟ Valor por compras: 5"));
+    mensaje = parseInt(prompt("seleccione una opcion" + "\n🛒 (1) Compra" + "\n💵 (2) Total de compras hoy" + "\n🎈 (3) Valor de ultima compra" + "\n🧲 (4) cantidad de compras: 4"));
 
     switch (mensaje) {
 
@@ -159,17 +144,22 @@ function menu() {
 
         case 2:
 
+            alert("el valor de todas las compras de hoy es " + totalHoy);
+            menu();
+
             break;
 
         case 3:
+
+            alert("el valor de la ultima compra de hoy fue " + total);
+            menu();
 
             break;
 
         case 4:
 
-            break;
-
-        case 5:
+            alert("la cantidad de compras realizadas hoy fue de " + cantidadCompras);
+            menu();
 
             break;
 
